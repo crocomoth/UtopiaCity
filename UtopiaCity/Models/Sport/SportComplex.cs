ba356d00ffbddb706b4df4abe466263bff67602b
AnatoliyDubovtsev <anatoliy.dubovtsev01@gmail.com>
2021-07-08 09:14:07 +0600

    Added data annotations for SportComplex model

--- UtopiaCity/Models/Sport/SportComplex.cs
+++ UtopiaCity/Models/Sport/SportComplex.cs
@@ -1,18 +1,34 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using UtopiaCity.Models.Sport.Enums;
 
 namespace UtopiaCity.Models.Sport
 {
+    /// <summary>
+    /// Represents sport complex.
+    /// </summary>
     public class SportComplex
     {
         public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"[^' '][a-zA-Z0-9' ']{1,100}[^' ']", ErrorMessage = "Enter correct title")]
         public string Title { get; set; }
+
+        [Required]
+        [Range(1, 100000)]
         public int NumberOfSeats { get; set; }
+
+        [Required]
         public DateTime BuildDate { get; set; }
+
+        [Required]
         public TypesOfSport TypeOfSport { get; set; }
 
         //TODO Take Address as a class
         /*public Address Address {get;set;}*/
+        [Required]
+        [RegularExpression(@"[^' '][a-zA-Z0-9' ']{1,100}[^' ']", ErrorMessage = "Enter correct address")]
         public string Address { get; set; }
     }
 }
