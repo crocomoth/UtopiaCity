399a4c534b6fb4fcad81e1997b1e6147d1480f5d
Danyello <ulkiora.panda@gmail.com>
2021-07-14 17:17:01 +0600

    added DataCapture model for all layers :(

---
+++ UtopiaCity/Models/Media/DataCapture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UtopiaCity.Models.Media
+{
+    public class DataCapture
+    {
+        [ScaffoldColumn(false)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id {  get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Header { get; set; }
+
+        [Required]
+        [MinLength(322)]
+        public string Content { get; set; }
+
+        [Required]
+        public string AuthorId { get; set; } // who wrote an information
+
+        [ScaffoldColumn(false)]
+        public bool? Status { get; set; } = true; // True - waiting | False = declined
+
+        [Required]
+        public bool Ready { get; set; } // True - ready to moderators check | False - in developement
+
+        [ScaffoldColumn(false)]
+        public bool Published { get; set; } = false;
+    }
+}
