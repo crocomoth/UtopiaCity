ce057d2702070eb6ce0535d47d031a7217489722
mendybayevalmas50 <mendybayevalmas50@gmail.com>
2021-07-15 01:18:23 +0600

    Первый тестовый коммит. Перекинуть отдельный проект в 1 общий  . В сервисе доделать TimelineContext, и вместе с ним модель

---
+++ UtopiaCity/Controllers/Timeline/TimelineBasicController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UtopiaCity.Controllers.Timeline
+{
+    public class TimelineBasicController : Controller
+    {
+
+    }
+}
---
+++ UtopiaCity/Services/Timeline/TimelineService.cs
@@ -0,0 +1,12 @@
+﻿namespace UtopiaCity.Services.Timeline
+{
+    public class TimelineService
+    {
+        private readonly TimelineContext _dbContext;
+
+        public TimelineService(TimelineContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
