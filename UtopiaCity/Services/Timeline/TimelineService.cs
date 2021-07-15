53ba63aee7fbc179a41a74988e4e04180e6fad21
mendybayevalmas50 <mendybayevalmas50@gmail.com>
2021-07-15 14:29:21 +0600

    Доделал сервис и модель. Осталось страницы и контроллеры.

--- UtopiaCity/Services/Timeline/TimelineService.cs
+++ UtopiaCity/Services/Timeline/TimelineService.cs
@@ -1,4 +1,8 @@
-﻿using UtopiaCity.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UtopiaCity.Data;
+using UtopiaCity.Models.TimelineModel;
 
 namespace UtopiaCity.Services.Timeline
 {
@@ -10,5 +14,58 @@ namespace UtopiaCity.Services.Timeline
         {
             _dbContext = dbContext;
         }
+
+        /// <summary>
+        /// CREATE METHOD 
+        /// </summary>
+        /// <param name="newEvent">MODEL OF EVENT</param>
+        /// <returns>NEW EVENT</returns>
+        public async Task CreateNewEvent(TimelineModel newEvent)
+        {
+            _dbContext.Add(newEvent);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// EDIT EVENT
+        /// </summary>
+        /// <param name="editedEvent">EVENT</param>
+        /// <returns>EDITED EVENT</returns>
+        public async Task EditEvent(TimelineModel chosenEvent)
+        {
+            _dbContext.Update(chosenEvent);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// DELETE(REMOVE) EVENT
+        /// </summary>
+        /// <param name="chosenEvent">EVENT</param>
+        /// <returns>NEW LIST WITH OUR 1 EVENT</returns>
+        public async Task DeleteEvent(TimelineModel chosenEvent)
+        {
+            _dbContext.Remove(chosenEvent);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// SHOW EVENT BY ID
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <returns>EVENT OR NOTHING</returns>
+        public async Task<TimelineModel> GetEventById(string id)
+        {
+            return await _dbContext.TimelineModel.FirstOrDefaultAsync(e => e.ID.Equals(id));
+        }
+
+        /// <summary>
+        /// SHOW LIST OF EVENT
+        /// </summary>
+        /// <returns>LIST</returns>
+        public async Task<List<TimelineModel>> GetList()
+        {
+            return await _dbContext.TimelineModel.ToListAsync();
+        }
+
     }
 }
