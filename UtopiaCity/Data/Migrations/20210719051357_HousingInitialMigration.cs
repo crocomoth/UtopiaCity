1d4e6e9a592156e4ee045d705dc4c66c9ceb45f9
madiyarkoishibekov <madiyarkoishibekov@gmail.com>
2021-07-19 11:57:14 +0600

    Add migration from housing model

---
+++ UtopiaCity/Data/Migrations/20210719051357_HousingInitialMigration.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UtopiaCity.Data.Migrations
+{
+    public partial class HousingInitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RealEstate",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Type = table.Column<int>(nullable: false),
+                    Address = table.Column<string>(nullable: true),
+                    Owner = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RealEstate", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Resident",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    RealEstateId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resident", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Resident_RealEstate_RealEstateId",
+                        column: x => x.RealEstateId,
+                        principalTable: "RealEstate",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resident_RealEstateId",
+                table: "Resident",
+                column: "RealEstateId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Resident");
+
+            migrationBuilder.DropTable(
+                name: "RealEstate");
+        }
+    }
+}
