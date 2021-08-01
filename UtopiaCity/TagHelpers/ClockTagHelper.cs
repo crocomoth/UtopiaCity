d5c2c7796ff44a355e47c901672785fb8cb186d2
mendybayevalmas50 <mendybayevalmas50@gmail.com>
2021-08-01 12:39:07 +0600

    FIX SqlException: Invalid object name 'ScheduleModel'. Added simple test clock tagHelper.Must be real time

---
+++ UtopiaCity/TagHelpers/ClockTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace UtopiaCity.TagHelpers{
+    public class ClockTagHelper : TagHelper
+{
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "time";
+        var currentTime = DateTime.Now.ToString("hh:f");
+        //string datetime = DateTime.Now.ToString("hh:f");
+        //output.Attributes.SetAttribute("datetime=" + datetime);
+        output.Content.SetContent(currentTime);
+    }
+}
+}
