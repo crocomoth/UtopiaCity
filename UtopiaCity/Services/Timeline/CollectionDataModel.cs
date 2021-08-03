856e48de44b9c18ee64d769a50f7ba01876fb40e
mendybayevalmas50 <mendybayevalmas50@gmail.com>
2021-08-04 00:44:58 +0600

    Adde new class for some models(e.g Airport). ScheduleListView had to remove.

---
+++ UtopiaCity/Services/Timeline/CollectionDataModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UtopiaCity.Models.Airport;
+
+namespace UtopiaCity.Services.Timeline
+{
+    public class CollectionDataModel
+    {
+        public List<Flight> Flight { get; set; }
+    }
+}
