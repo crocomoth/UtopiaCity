224429a661519e478601147531a255cf99d93bdb
Анатолий Дубовцев <anatoliy.dubovtsev01@gmail.com>
2021-08-06 23:37:58 +0600

    Added controllers and views for TransportManager. Added enums for TransportManagerSystem

---
+++ UtopiaCity/Models/Airport/TransportManagerSystem/Enums/TypeOfPay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UtopiaCity.Models.Airport.TransportManagerSystem.Enums
+{
+    public enum TypeOfPay
+    {
+        cash = 0,
+        eMoney = 1,
+        webMoney = 2,
+        BankService = 3
+    }
+}
---
+++ UtopiaCity/Models/Airport/TransportManagerSystem/Enums/TypeOfTransportForPassengers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UtopiaCity.Models.Airport.TransportManagerSystem.Enums
+{
+    public enum TypeOfTransportForPassengers
+    {
+        Car = 0,
+        Bus = 1,
+        AirportSpecialBus = 2
+    }
+}
