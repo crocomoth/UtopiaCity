44241cc9587a77b96d3c07ef146392550d6b43d5
Анатолий Дубовцев <anatoliy.dubovtsev01@gmail.com>
2021-08-06 20:08:41 +0600

    Added Event model and migration for it. Added EventTypeItem model

--- UtopiaCity/Data/ApplicationDbContext.cs
+++ UtopiaCity/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using UtopiaCity.Models.Airport;
 using UtopiaCity.Models.CityAdministration;
 using UtopiaCity.Models.Emergency;
+using UtopiaCity.Models.Life;
 using UtopiaCity.Models.Sport;
 
 namespace UtopiaCity.Data
@@ -18,5 +19,6 @@ namespace UtopiaCity.Data
         public DbSet<WeatherReport> WeatherReports { get; set; }
         public DbSet<RersidentAccount> RersidentAccount { get; set; }
         public DbSet<SportComplex> SportComplexes { get; set; }
+        public DbSet<Event> Events { get; set; }
     }
 }
