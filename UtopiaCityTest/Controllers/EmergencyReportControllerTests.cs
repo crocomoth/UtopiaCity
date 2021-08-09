aed2d140c75e973f6ffa152ee92580820d181027
crocomoth <justletmeplaymf@gmail.com>
2021-08-09 10:16:58 +0300

    add tests

--- UtopiaCityTest/Controllers/EmergencyReportControllerTests.cs
+++ UtopiaCityTest/Controllers/EmergencyReportControllerTests.cs
@@ -11,18 +11,27 @@ using Xunit;
 
 namespace UtopiaCityTest.Controllers
 {
-    public class EmergencyReportControllerTests
+    public class EmergencyReportControllerTests : IDisposable
     {
+        private EmergencyReportController controller;
+        private Mock<EmergencyReportService> serviceMock;
+
+        public EmergencyReportControllerTests()
+        {
+            var applicationDbContextMock = new Mock<ApplicationDbContext>(new DbContextOptions<ApplicationDbContext>());
+            serviceMock = new Mock<EmergencyReportService>(applicationDbContextMock.Object);
+            controller = new EmergencyReportController(serviceMock.Object);
+        }
+
+        public void Dispose()
+        {
+        }
+
         [Fact]
         public void Index_ReturnsEmptyView_WhenThereIsNoModel()
         {
             // arrange
-            var applicationDbContextMock = new Mock<ApplicationDbContext>(new DbContextOptions<ApplicationDbContext>());
-            var serviceMock = new Mock<EmergencyReportService>(applicationDbContextMock.Object);
-
             serviceMock.Setup(x => x.GetEmergencyReports()).ReturnsAsync(() => new List<EmergencyReport>());
-
-            var controller = new EmergencyReportController(serviceMock.Object);
 
             // act
             var result = controller.Index().GetAwaiter().GetResult();
@@ -31,6 +40,33 @@ namespace UtopiaCityTest.Controllers
             var viewResult = result as ViewResult;
             Assert.True(viewResult != null);
             Assert.True(viewResult.ViewName == "EmergencyReportListView");
+        }
+
+        [Fact]
+        public void Index_ReturnsViewWithModel_WhenThereIsModel()
+        {
+            // arrange
+            var model = new EmergencyReport
+            {
+                Id = "1",
+                Report = "Report",
+                ReportTime = DateTime.Now
+            };
+
+            serviceMock.Setup(x => x.GetEmergencyReports()).ReturnsAsync(() => new List<EmergencyReport> { model });
+
+            // act
+            var result = controller.Index().GetAwaiter().GetResult();
+
+            // assert
+            var viewResult = result as ViewResult;
+            Assert.True(viewResult != null);
+            Assert.True(viewResult.ViewName == "EmergencyReportListView");
+
+            var viewModel = viewResult.Model as List<EmergencyReport>;
+            Assert.True(viewModel != null);
+            Assert.True(viewModel.Count == 1);
+            Assert.Equal(model, viewModel[0]);
         }
     }
 }
