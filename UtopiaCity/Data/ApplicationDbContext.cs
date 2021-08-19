c75559a894664eb718a8a865f66be63a04591ad1
AnatoliyDubovtsev <anatoliy.dubovtsev01@gmail.com>
2021-08-19 11:56:44 +0600

    I fixed error with brackets in the "ModelSnapshot". Fixed "readonly" duplication in the "CitizenAccountController". Added "SportTicket" model and migration for it

--- UtopiaCity/Data/ApplicationDbContext.cs
+++ UtopiaCity/Data/ApplicationDbContext.cs
@@ -25,6 +25,21 @@ namespace UtopiaCity.Data
                 .WithMany(e => e.SportEvents)
                 .OnDelete(DeleteBehavior.Cascade);
 
+            modelBuilder.Entity<SportTicket>()
+                .HasOne(s => s.SportComplex)
+                .WithMany(t => t.SportTickets)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<SportTicket>()
+                .HasOne(s => s.SportEvent)
+                .WithMany(s => s.SportTickets)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<SportTicket>()
+                .HasOne(r => r.AppUser)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Cascade);
+
             base.OnModelCreating(modelBuilder);
         }
 
@@ -44,6 +59,6 @@ namespace UtopiaCity.Data
         public DbSet<SportEvent> SportEvents { get; set; }
         public DbSet<AppUser> AppUser { get; set; }
         public DbSet<CitizensTask> CitizensTasks { get; set; }
-
+        public DbSet<SportTicket> SportTickets { get; set; }
     }
 }
---
+++ UtopiaCity/Models/Sport/SportTicket.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using UtopiaCity.Models.CitizenAccount;
+
+namespace UtopiaCity.Models.Sport
+{
+    public class SportTicket
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string TicketId { get; set; }
+
+        [Required]
+        [ForeignKey("SportComplex")]
+        public string SportComplexId { get; set; }
+
+        [Required]
+        [ForeignKey("SportEvent")]
+        public string SportEventId { get; set; }
+
+        [Required]
+        [ForeignKey("AppUser")]
+        public string AppUserId { get; set; }
+
+        public SportComplex SportComplex { get; set; }
+        public SportEvent SportEvent { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
