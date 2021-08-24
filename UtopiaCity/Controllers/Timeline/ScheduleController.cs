30eff7a0392f49d72e2ba27391e6f7da01663fa8
mendybayevalmas50 <mendybayevalmas50@gmail.com>
2021-08-24 09:00:32 +0600

    Remove spaces, and add little filter with stirng param in command line in top

--- UtopiaCity/Controllers/Timeline/ScheduleController.cs
+++ UtopiaCity/Controllers/Timeline/ScheduleController.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using System.Threading.Tasks;
-using UtopiaCity.Services.Airport;
-using UtopiaCity.Models.Airport;
+using UtopiaCity.Data;
+using UtopiaCity.Models.TimelineModel.CollectionDataModel;
 using UtopiaCity.Services.Timeline;
 
 namespace UtopiaCity.Controllers.Timeline
@@ -9,17 +10,28 @@ namespace UtopiaCity.Controllers.Timeline
     public class ScheduleController : Controller
     {
         private readonly ScheduleService _scheduleService;
+        private readonly TimelineService _timelineService;
+        ApplicationDbContext _context;
 
-        public ScheduleController(ScheduleService scheduleService)
+        public ScheduleController(ScheduleService scheduleService, TimelineService timelineService, ApplicationDbContext context)
         {
             _scheduleService = scheduleService;
+            _timelineService = timelineService;
+            _context = context;
         }
 
         //LIST
-        public async Task<IActionResult> Index()
+        //public async Task<IActionResult> Index()
+        //{
+        //    return View("ScheduleListView", await _scheduleService.GetFlights());
+        //}
+
+        public ViewResult Index(string name) => View(new CommonModel
         {
-            return View("ScheduleListView", await _scheduleService.GetFlights());
-        }
+            Flight = _context.Flights
+            .Where(i => i.Weather == name)
+            .OrderBy(i => i.Id)
+        });
 
     }
 }
---
+++ UtopiaCity/Models/TimelineModel/CollectionDataModel/CommonModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UtopiaCity.Models.Airport;
+
+namespace UtopiaCity.Models.TimelineModel.CollectionDataModel
+{
+    public class CommonModel
+    {
+        public IEnumerable<Flight> Flight { get; set; }
+    }
+}
