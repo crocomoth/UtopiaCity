b130e8c66b6d0d07e28f30d44d277ab5ea583531
Kenes Kuanyshov <kuanyshovk@gmail.com>
2021-09-07 15:51:37 +0600

    Added smtp in company controller

---
+++ UtopiaCity/Common/EmailService.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace UtopiaCity.Common
+{
+    public class EmailService
+    {
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("UtopiaCity", "UtopiaCity@mail.ru"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.mail.ru", 465, true);
+                await client.AuthenticateAsync("yourEmail@mail.ru", "yourPassword");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
--- UtopiaCity/Controllers/Business/CompanyController.cs
+++ UtopiaCity/Controllers/Business/CompanyController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UtopiaCity.Common;
 using UtopiaCity.Models.Business;
 using UtopiaCity.Services.Business;
 using UtopiaCity.Utils;
@@ -40,6 +41,11 @@ namespace UtopiaCity.Controllers.Business
                 company.IIK = "KZ" + RandomUtil.GenerateRandomString(18);
                 company.BIN = RandomUtil.GenerateRandomString(12);
                 await _companyAppService.Create(company);
+
+                EmailService emailService = new EmailService();
+                await emailService.SendEmailAsync("UserEmail", "UtopiaCity-Business",
+                    $"Congratulations! you have registered a new company, your IIK: {company.IIK}, your BIN: {company.BIN}");
+
                 return RedirectToAction(nameof(Index));
             }
 
