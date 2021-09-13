d3a821ed2c339db8f5efd568420c850f52597eca
mendybayevalmas50 <mendybayevalmas50@gmail.com>
2021-09-13 14:57:07 +0600

    Removed all breascrums. And added very simple email sender

---
+++ UtopiaCity/Controllers/MailSenderController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace UtopiaCity.Controllers
+{
+    public class MailSenderController: Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string SendEmail(string Name, string Email, string Message)
+        {
+
+            try
+            {
+                // Credentials
+                var credentials = new NetworkCredential("2681567f7e14e0", "d4bb031a8ee6b3");
+                // Mail message
+                var mail = new MailMessage()
+                {
+                    From = new MailAddress("noreply@codinginfinite.com"),
+                    Subject = "Email Sender App",
+                    Body = Message
+                };
+
+                mail.IsBodyHtml = true;
+                mail.To.Add(new MailAddress(Email));
+                // Smtp client
+                var client = new SmtpClient()
+                {
+                    Port = 587,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Host = "smtp.mailtrap.io",
+                    EnableSsl = true,
+                    Credentials = credentials
+                };
+                client.Send(mail);
+                return "Email Sent Successfully!";
+            }
+            catch (System.Exception e)
+            {
+                return e.Message;
+            }
+
+        }
+    }
+}
